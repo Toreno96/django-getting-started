8c7ba38ff0d9c97d740060bfd7f18fe0471b00c4
Toreno96 <toreno96@gmail.com>
2019-06-18 20:21:19 +0200

    Add experimental logging
    To check basics of how it works in Django.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,8 @@
+import logging
+
 from django.http import HttpResponse
 
 
 def index(request, **kwargs):
+    logging.getLogger('django.server').info(kwargs)
     return HttpResponse("Hello, world. You're at the polls index.")
