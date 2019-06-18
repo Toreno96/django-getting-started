2de8cb8b1c411adb47cd99f074eceb953d2a50c5
Toreno96 <toreno96@gmail.com>
2019-06-18 22:37:44 +0200

    Add new (placeholder) views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, {'spam': 'egg'}, name='index'),
+    # ex: /polls/
+    path('', views.index, name='index'),
+    path('<int:question_id>/', include([
+        # ex: /polls/5/
+        path('', views.detail, name='detail'),
+        # ex: /polls/5/results/
+        path('results/', views.results, name='results'),
+        # ex: /polls/5/vote/
+        path('vote/', views.vote, name='vote'),
+    ])),
 ]
