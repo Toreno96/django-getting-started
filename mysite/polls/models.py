77fdde9b4f3c3b53871ababf93025cf155cfe97f
Toreno96 <toreno96@gmail.com>
2019-06-19 20:10:23 +0200

    Bugfix
    `was_published_recently` was returning True for questions whose
    pub_date was in the future.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,7 +10,8 @@ class Question(models.Model):
         return f'{self.question_text}, {self.pub_date}'
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - timezone.timedelta(days=1)
+        now = timezone.now()
+        return now - timezone.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
